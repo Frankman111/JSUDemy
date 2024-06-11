82fa0ceb3934ff7410507e8d061e34f33daa53b0
Frankman111 <haider.frank@gmail.com>
2024-06-11 13:30:55 +0200

    functions

---
+++ arrays.js
@@ -0,0 +1,16 @@
+// ## Arrays #4
+
+// 1. create "fruits" array and store some fruit values
+// 2. setup the last item as number (random)
+// 3. assign first fruit to the variable
+// 4. re-assign last array item to the actual fruit
+// 5. log both first fruit variable and entire fruits array
+
+const fruits =["apple", "banana", "orange", 45 ];
+
+let fruit = fruits[0];
+
+fruits[0] = 45;
+
+console.log(fruit);
+console.log(fruits);
