4fff1d9bd932f4e8df4074f76645d2ad0fae9518
Frankman111 <haider.frank@gmail.com>
2024-06-18 09:13:00 +0200

    map

--- myChallenges/map.js
+++ myChallenges/map.js
@@ -21,3 +21,22 @@ const people = [
     }
 ]
 
+const ages = people.map(function(person){
+    return person.age + 20;
+});
+
+const newPeople = people.map(function(person){
+    return {
+        firstName: person.name.toUpperCase(),
+        oldAge: person.age + 20
+    };
+});
+
+const names = people.map(function (person) {
+    return `<h1>${person.name}</h1>`
+    
+})
+
+document.body.innerHTML = names.join("")
+
+console.log(names);
