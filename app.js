daed2614c81729f5daf4dba0357bee552f765450
Frankman111 <haider.frank@gmail.com>
2024-06-21 12:08:44 +0200

    lastUpdate

--- app.js
+++ app.js
@@ -7,3 +7,20 @@
 // firstChild
 // lastChild
 
+const result = document.querySelector("#result");
+
+const allChildren = result.childNodes;
+// console.log(allChildren);
+
+const children = result.children;
+// console.log(children);
+
+const firstChildren = result.firstChild;
+// console.log(firstChildren);
+
+const lastChildren = result.lastChild;
+// console.log(lastChildren);
+
+
+// Tutorial parentElement
+
