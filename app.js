745106b43ba7887bb2d504dae0308557c8ef5868
Frankman111 <haider.frank@gmail.com>
2024-06-21 10:14:27 +0200

    DOM+element_selector

---
+++ app.js
@@ -0,0 +1,21 @@
+// window object = browser api
+// document
+// console.dir
+//alert("Hello")
+console.dir(document);
+
+//select the element or group of elements the we want 
+// decide the effect we want to apply to the selection
+
+//getElementById("element")
+
+const h1 = document.getElementById("title");
+
+h1.style.color = "red";
+
+const btn = document.getElementById("btn")
+
+btn.style.backgroundColor = "blue";
+
+btn.style.color = "white";
+
---
+++ myChallenges/math_object.js
@@ -0,0 +1,25 @@
+//MAth
+//Standard built-im objects - always available
+
+
+// rounds down to 4
+// const number = 4.56789;
+// const result = Math.floor(number);
+
+// const number = 4.56789;
+// const result = Math.ceil(number);
+
+// const number = 25;
+// const result = Math.sqrt(number);
+
+// const result = Math.PI;
+
+// const result = Math.min(4,5,6,7,8,9);
+
+// const result = Math.max(4,5,6,7,8,9);
+
+//const result = Math.floor(Math.random()*10); //from 0 - 9
+//const result = Math.ceil(Math.random()*10); //from 1 - 10
+// const result = Math.floor(Math.random()*10 + 1); //from 1 - 10
+
+console.log(result);
