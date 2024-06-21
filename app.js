8a1efd2262ae001e3c5e3684ca3564c28bd5197c
Frankman111 <haider.frank@gmail.com>
2024-06-21 11:08:18 +0200

    getElementsByTagName

--- app.js
+++ app.js
@@ -2,20 +2,53 @@
 // document
 // console.dir
 //alert("Hello")
-console.dir(document);
+//console.dir(document);
 
+// Tutorial getElementById
 //select the element or group of elements the we want 
 // decide the effect we want to apply to the selection
 
 //getElementById("element")
 
-const h1 = document.getElementById("title");
+// const h1 = document.getElementById("title");
 
-h1.style.color = "red";
+// h1.style.color = "red";
 
-const btn = document.getElementById("btn")
+// const btn = document.getElementById("btn")
 
-btn.style.backgroundColor = "blue";
+// btn.style.backgroundColor = "blue";
 
-btn.style.color = "white";
+// btn.style.color = "white";
 
+
+
+// Tutorial getElementsByTagName
+
+// getElementsByTagName("tagname")
+// HTMLCollection = array-like object
+// index, length property but not array methods (forEach)
+
+// const headings = document.getElementsByTagName("h2");
+// headings[0].style.color = "red";
+// console.log(headings.length);
+
+//const items = document.getElementsByTagName("li");
+// items.forEach(function(item)
+// {
+//     //returns item.forEach is not a function
+// });
+
+// querySelectorAll() in most cases
+// NodeList - objects are collections of nodes
+// can run forEach
+// turn them into array - spread operator [...]
+// after that can use any array properties
+
+// const betterItems = [...items];
+
+// betterItems.forEach(function(item){
+//     console.log(item);
+// })
+
+// console.log(items);
+// console.log(betterItems);
