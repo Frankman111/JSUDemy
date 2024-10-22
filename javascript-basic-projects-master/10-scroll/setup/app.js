d4ad5c55cf3af7991afde2f59700ebb31bea8944
Frankman111 <haider.frank@gmail.com>
2024-10-22 15:19:02 +0200

    scroll_project_navbar_ready

--- javascript-basic-projects-master/10-scroll/setup/app.js
+++ javascript-basic-projects-master/10-scroll/setup/app.js
@@ -5,9 +5,33 @@
 
 // ********** set date ************
 
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
 // ********** close links ************
 
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function(){
+    //linksContainer.classList.toggle("show-links"); //show-links is a fix height - when the nav items are changing you either have a gap or aa hidden item
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+
+if(containerHeight === 0){
+    linksContainer.style.height = `${linksHeight}px`
+}
+else{
+    linksContainer.style.height = 0;
+}
+    
+    
+})
+
 // ********** fixed navbar ************
+
+
 
 // ********** smooth scroll ************
 // select links
