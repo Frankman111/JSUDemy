420440449406bf3f9b4584bc95dc9b821b6db1bf
Frankman111 <haider.frank@gmail.com>
2024-10-25 09:19:53 +0200

    scroll_project_done

--- javascript-basic-projects-master/10-scroll/setup/app.js
+++ javascript-basic-projects-master/10-scroll/setup/app.js
@@ -25,13 +25,67 @@ if(containerHeight === 0){
 else{
     linksContainer.style.height = 0;
 }
-    
-    
 })
+
+
 
 // ********** fixed navbar ************
 
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
 
+window.addEventListener("scroll", function(){
+    // console.log(window.scrollY);
+    const scrollHeight = window.scrollY;
+    const navHeight = navbar.getBoundingClientRect().height;
+
+    if (scrollHeight > navHeight){
+        navbar.classList.add("fixed-nav");
+    }
+    else{
+        navbar.classList.remove("fixed-nav");
+    }
+    if(scrollHeight > 500){
+        topLink.classList.add("show-link");
+    }
+    else{
+        topLink.classList.remove("show-link");
+    }
+})
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function(link){
+    link.addEventListener("click",function(e){
+        //prevent default
+        e.preventDefault();
+
+        // navigate to specific spot
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        //console.log(id);
+
+        const element = document.getElementById(id);
+        // calculate the heights
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains("fixed-nav");
+        let position = element.offsetTop - navHeight;
+
+        if(!fixedNav){
+            position = position - navHeight; //works in fixed Navbar
+        }
+        if(navHeight > 82){
+            position = position + containerHeight; //works in Hamburger Navbar
+        }
+
+
+        //console.log(position); shows the position of the position-id
+        window.scrollTo({
+            left:0,
+            top: position,
+        });
+        linksContainer.style.height = 0;
+    });
+});
