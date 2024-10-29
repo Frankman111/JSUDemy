fde70453de5964e16d4d76199635cf59ce1caf3e
Frankman111 <haider.frank@gmail.com>
2024-10-29 11:34:16 +0100

    date_tutorial_fin

--- javascript-basic-projects-master/12-countdown-timer/setup/app.js
+++ javascript-basic-projects-master/12-countdown-timer/setup/app.js
@@ -26,8 +26,16 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2025,4,24,11,30,0);
-//console.log(futureDate);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth  = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+
+//let futureDate = new Date(2024,9,29,11,45,0);
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 45, 0)
+
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -39,14 +47,57 @@ const date = futureDate.getDate();
 
 const weekday = weekdays[futureDate.getDay()]; // gives back a number
 
-
-
 giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}`;
 
 
+// future time in ms
+const futureTime = futureDate.getTime();
 
 
+function getRemainingTime(){
+  const today = new Date().getTime();
+  const t = futureTime - today;
 
+  const oneDay = 24*60*60*1000;
+  const oneHour = 60*60*1000;
+  const oneMinute = 60*1000;
+
+  let days = t/oneDay;
+  days = Math.floor(days);
+
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  // set Values array
+
+  const values = [days, hours, minutes, seconds];
+
+  
+  function format(item){
+    if(item < 10){
+      return item = `0${item}`
+    }
+    return item
+  }
+
+  items.forEach(function(item, index){
+    item.innerHTML = format(values[index]);
+  });
+  
+  if (t < 0){
+    clearInterval(countdown);
+    deadline.innerHTML =`<h4 class="expired">This giveaway has expired</h4>`
+  }
+
+}
+
+//countdown 
+
+let countdown = setInterval(getRemainingTime, 1000); 
+
+
+getRemainingTime();
 
 
 
